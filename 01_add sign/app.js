91515f02f644319c902dfd5dabdd3f07d9dbe231
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-08-16 20:00:11 +0200

    add sign 1st version

--- 01_add sign/app.js
+++ 01_add sign/app.js
@@ -1,1 +1,24 @@
-//
+//Przycisk - po kliknięciu dodawana jest litera do tekstu
+class App extends React.Component {
+  state = {
+    text: "",
+  };
+
+  handleClick = () => {
+    // alert(this.state.text);
+    const letter = "A";
+    this.setState({
+      text: this.state.text + letter,
+    });
+  };
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>Dodaj "A"</button>
+        <h1>{this.state.text}</h1>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
