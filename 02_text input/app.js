46a7dda5585f01eab0fde0038ddf9306c068b60b
Michał Maciorowski <michalma@ThinkStation-S30-cd1582d8.localdomain>
2021-08-29 20:19:50 +0200

    text input project completed

--- 02_text input/app.js
+++ 02_text input/app.js
@@ -1,1 +1,36 @@
 //
+class App extends React.Component {
+  state = {
+    value: "",
+  };
+
+  handleInputChange(e) {
+    //  console.log(e.target.value);
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  handleResetClick = () => {
+    this.setState({
+      value: "",
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <input
+          value={this.state.value}
+          placeholder="Wpisz coś"
+          onChange={this.handleInputChange.bind(this)}
+          type="text"
+        />
+        <button onClick={this.handleResetClick}>Reset</button>
+        <h1 className="title">{this.state.value.toLocaleUpperCase()}</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
