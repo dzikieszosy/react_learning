0d632b59bcb1089934d823adcccc516712095cf9
Michał Maciorowski <dzikieszosy@gmail.com>
2021-09-19 10:10:00 +0200

    restaurant menu completed

--- 08_restaurant_menu/Item.js
+++ 08_restaurant_menu/Item.js
@@ -1,1 +1,10 @@
-const Item = (props) => <li>{props.name}</li>;
+const Item = (props) => (
+  <li
+    className={props.active ? "enabled" : "disabled"}
+    onClick={() => props.changeStatus(props.id)}
+  >
+    {props.name}
+  </li>
+);
+
+// style={props.active ? { fontWeight: "bold" } : { color: "gray" }}
